3e91e97318ad726c2056159d37388db0eafe5681
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2023-01-11 15:04:47 +0530

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import "express-async-errors";
+
+import {configureDB} from "./utils/config.js";
+import {errorHandler, tokenExtractor, tokenValidator, unknownEndpoint, validateToken} from "./utils/middleware.js";
+import userRouter from "./routes/userRouter.js";
+
+const app = express()
+
+await configureDB()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(morgan('dev'))
+
+app.use('/api/auth', userRouter)
+
+app.use(tokenExtractor)
+app.use(tokenValidator)
+
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
+export default app
